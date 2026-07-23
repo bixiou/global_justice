3f5650d47139043238edfd1c8723373b98e7ef57
bixiou <fabre.adrien@hotmail.fr>
2026-07-23 18:53:07 +0200

    first results

--- code_simulator/IT_survey/conjoint_any.js
+++ code_simulator/IT_survey/conjoint_any.js
@@ -163,7 +163,7 @@ function renderCaption(capId, brackets, annualValues, region) {
 // Beef & flights phrase text.
 const BF_TXT = {
   none:    "Nessuna variazione",
-  beef:    "Meno prodotti bovini (2 porzioni di carne al mese)",
+  beef:    "Meno prodotti bovini (2 porzioni di carne al mese)<br>Prezzi triplicati",
   flights: "Meno voli (2.500 km all'anno)<br>Prezzi triplicati",
   both:    "Meno prodotti bovini (2 porzioni di carne al mese)<br>Meno voli (2.500 km all'anno)<br>Prezzi triplicati"
 };
